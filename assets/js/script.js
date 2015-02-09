93c1a166ebecb00905e2a78aa376e27c56cc2576
rwsong <rw_Song@sina.com>
2015-02-09 20:38:55 +0800

    测试文章

--- assets/js/script.js
+++ assets/js/script.js
@@ -134,7 +134,7 @@ function afterPjax() {
   var ds_loaded = false,
       top = $('#disqus_thread').offset().top;
       identifier = $('#post__title').data('identifier');
-  window.disqus_shortname = '';
+  window.disqus_shortname = '小宋';
   window.disqus_identifier = identifier;
 
   function check() {
